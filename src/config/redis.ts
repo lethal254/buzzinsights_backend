8558ce96a4427968ea1718ff1b338b1efa955f62
Benard Mangheni (Techno Brain Global FZ LLC) <benardogutu65@gmail.com>
2025-03-11 01:26:05 +0300

    Done

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,17 +1,27 @@
 const Redis = require("ioredis")
 
-export const redis = new Redis({
+const redis = new Redis({
   host: process.env.AZURE_CACHE_FOR_REDIS_HOST_NAME || "localhost",
-  port: process.env.AZURE_CACHE_FOR_REDIS_PORT || 6379,
+  port: 6380,
   password: process.env.AZURE_CACHE_FOR_REDIS_ACCESS_KEY,
-  ...(process.env.AZURE_CACHE_FOR_REDIS_PORT === "6380"
-    ? {
-        tls: {
-          servername: process.env.AZURE_CACHE_FOR_REDIS_HOST_NAME,
-        },
-      }
-    : {}),
+  tls: {
+    servername: process.env.AZURE_CACHE_FOR_REDIS_HOST_NAME,
+  },
   enableTLSForSentinelMode: false,
   maxRetriesPerRequest: null, // Disable request retries
   enableReadyCheck: false, // Skip readiness check to prevent issues
 })
+
+// Log connection success or fails
+redis.on("connect", () => {
+  console.log("Redis connected")
+})
+interface RedisErrorHandler {
+  (error: Error): void
+}
+
+redis.on("error", (error: Error): void => {
+  console.error("Redis error", error)
+})
+
+export { redis }
