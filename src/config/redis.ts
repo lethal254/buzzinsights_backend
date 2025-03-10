93dccf6cabad57929b69dd188a30fe09e7d78aa7
Benard Mangheni (Techno Brain Global FZ LLC) <benardogutu65@gmail.com>
2025-03-11 00:37:40 +0300

    Done

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -2,11 +2,15 @@ const Redis = require("ioredis")
 
 export const redis = new Redis({
   host: process.env.AZURE_CACHE_FOR_REDIS_HOST_NAME || "localhost",
-  port: 6380,
+  port: process.env.AZURE_CACHE_FOR_REDIS_PORT || 6379,
   password: process.env.AZURE_CACHE_FOR_REDIS_ACCESS_KEY,
-  tls: {
-    servername: process.env.AZURE_CACHE_FOR_REDIS_HOST_NAME,
-  },
+  ...(process.env.AZURE_CACHE_FOR_REDIS_PORT === "6380"
+    ? {
+        tls: {
+          servername: process.env.AZURE_CACHE_FOR_REDIS_HOST_NAME,
+        },
+      }
+    : {}),
   enableTLSForSentinelMode: false,
   maxRetriesPerRequest: null, // Disable request retries
   enableReadyCheck: false, // Skip readiness check to prevent issues
